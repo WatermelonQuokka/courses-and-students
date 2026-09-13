9eeaa10b43aa92959c61a30e75d288e7fb3b032e
WatermelonQuokka <danielchis123@hotmail.com>
2026-09-13 15:37:46 +0200

    added enroll method with security and a testing phase inprogram

--- Course.cs
+++ Course.cs
@@ -1,6 +1,18 @@
 class Course(string name, int capacity)
 {
+    // public fields
     public string Name = name;
     public int Capacity = capacity;
     public List<Student> Students = [];
+    
+    //enroll method checking if course contains student
+    //if not add student to course and add course to student
+    public void Enroll(Student student)
+    {
+        if(!Students.Contains(student))
+        {
+            Students.Add(student);
+            student.Courses.Add(this);
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Course math = new("Mathematics", 5);
+Course eng = new("English", 5);
+Course phys = new("Physics", 5);
+Course sport = new("Sports", 5);
+
+Student anna = new("Anna Anderson");
+Student bert = new("Bert Bertson");
+Student carl = new("Carl Carlson");
+Student david = new("David Davidson");
+Student eric = new("Eric Ericson");
+Student filip = new("Filip Filipson");
+
+math.Enroll(anna);
+eng.Enroll(anna);
+phys.Enroll(anna);
+sport.Enroll(anna);
+math.Enroll(bert);
+math.Enroll(carl);
+math.Enroll(david);
+math.Enroll(eric);
+
+foreach (Course c in anna.Courses)
+{
+    Console.WriteLine(c.Name);
+}
+
+foreach (Student c in math.Students)
+{
+    Console.WriteLine(c.Name);
+}
